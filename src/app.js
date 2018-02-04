3203c9c44e80434d57e613210ef744b1d880bdf1
Zandraa Perjesi <zandraa.perjesi@gmail.com>
2018-02-04 20:45:02 +0100

    cd ..

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const serveStatic = require('serve-static')
 
 const app = express()
 app.use(morgan('combine'))
 app.use(bodyParser.json())
 app.use(cors())
+app.use(serveStatic(__dirname + "/dist"))
 
 app.get('/status', (req, res) => {
   res.send({
